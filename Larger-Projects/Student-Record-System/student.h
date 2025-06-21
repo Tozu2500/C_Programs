7898dd3e5535766dd207a55518a3d4029bcfd0ca
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2025-06-21 22:57:29 +0300

    Finish student.h

--- Larger-Projects/Student-Record-System/student.h
+++ Larger-Projects/Student-Record-System/student.h
@@ -1,27 +1,91 @@
 #ifndef STUDENT_H
 #define STUDENT_H
 
-#define MAX_NAME_LENGTH 50
-#define MAX_STUDENTS 100
-#define FILENAME "students.dat"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
+#define MAX_NAME_LENGTH 100
+#define MAX_EMAIL_LENGTH 100
+#define MAX_PHONE_LENGTH 20
+#define MAX_ADDRESS_LENGTH 200
+#define MAX_COURSE_LENGTH 50
+#define MAX_FILENAME_LENGTH 100
+#define MAX_STUDENTS 1000
+#define MAX_SUBJECTS 10
+
+// Grade struct
+typedef struct {
+    char subject[MAX_COURSE_LENGTH];
+    float grade;
+    int credits;
+} Grade;
+
+// Student
 typedef struct {
     int id;
-    char name[MAX_NAME_LENGTH];
-    int age;
+    char first_name[MAX_NAME_LENGTH];
+    char last_name[MAX_NAME_LENGTH];
+    char email[MAX_EMAIL_LENGTH];
+    char phone[MAX_PHONE_LENGTH];
+    char address[MAX_ADDRESS_LENGTH];
+    char course[MAX_COURSE_LENGTH];
+    int enrollment_year;
+    Grade grades[MAX_SUBJECTS];
+    int num_grades;
     float gpa;
-    char course[MAX_NAME_LENGTH];
-    char email[MAX_NAME_LENGTH];
-    char phone[20];
-    int enrollmentYear;
+    int is_active;
 } Student;
 
-// Function protos
-void addStudent(Student students[], int *count);
-void displayAllStudents(Student students[], int count);
-void searchStudent(Student students[], int count);
-void updateStudent(Student students[], int count);
-void deleteStudent(Student students[], int *count);
-void displayStudentInfo(Student student);
+// Database struct
+typedef struct {
+    Student students[MAX_STUDENTS];
+    int count;
+    int next_id;
+} StudentDatabase;
+
+// Function prototypes
+
+// Core database operations
+int init_database(StudentDatabase *db);
+int save_database(StudentDatabase *db, const char *filename);
+int load_database(StudentDatabase *db, const char *filename);
+void cleanup_database(StudentDatabase *db);
+
+// Student operations
+int add_student(StudentDatabase *db, Student *student);
+int update_student(StudentDatabase *db, int id, Student *updated_student);
+int delete_student(StudentDatabase *db, int id);
+float calculate_gpa(Student *student);
+void update_student_gpa(Student *student);
+
+// Search and filter operations
+int search_students_by_course(StudentDatabase *db, const char *course, Student results[], int max_results);
+int search_students_by_year(StudentDatabase *db, int year, Student results[], int max_results);
+int get_students_by_gpa_range(StudentDatabase *db, float min_gpa, float max_gpa, Student results[], int max_results);
+
+// Display and reports
+void display_students(const Student *student);
+void display_all_students(const StudentDatabase *db);
+void display_student_grades(const Student *student);
+void generate_transcript(const Student *student);
+void generate_class_report(StudentDatabase *db, const char *course);
+
+// Util functions
+int validate_email(const char *email);
+int validate_phone(const char *phone);
+void trim_whitespace(char *str);
+int get_current_year(void);
+
+// I/O Helpers
+void clear_input_buffer(void);
+int get_student_input(Student *student);
+void print_menu(void);
+
+// Sorting functions
+void sort_students_by_name(Student students[], int count);
+void sort_students_by_id(Student students[], int count);
+void sort_students_by_gpa(Student stdents[], int count);
 
 #endif // STUDENT_H
