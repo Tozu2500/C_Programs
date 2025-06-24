19af224dea2865a5d0bf3ce58d299cd8e37d93ac
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2025-06-24 03:44:03 +0300

    Add files via upload

---
+++ Larger-Projects/fileio.h
@@ -0,0 +1,10 @@
+#ifndef FILEIO_H
+#define FILEIO_H
+
+#include "student.h"
+
+// Function prototypes
+int loadStudents(Student students[]);
+void saveStudents(Student students[], int count);
+
+#endif // fileio.h
