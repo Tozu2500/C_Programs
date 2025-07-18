a5f5f65303d5d6413164742b2117ec4511f7c40f
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2025-07-19 00:31:26 +0300

    Add files via upload

---
+++ Useful-Programs/Windows_File_Explorer/file_explorer.h
@@ -0,0 +1,40 @@
+#ifndef FILE_EXPLORER_H
+#define FILE_EXPLORER_H
+
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define MAX_PATH_LEN 260
+#define MAX_FILES 1000
+
+typedef struct {
+    char name[MAX_PATH_LEN];
+    char full_path[MAX_PATH_LEN];
+    DWORD attributes;
+    LARGE_INTEGER file_size;
+    FILETIME last_modified;
+    int is_directory;
+} FileEntry;
+
+typedef struct {
+    char current_path[MAX_PATH_LEN];
+    FileEntry files[MAX_FILES];
+    int file_count;
+} FileExplorer;
+
+void init_explorer(FileExplorer *explorer);
+int list_directory(FileExplorer *explorer, const char *path);
+void display_files(const FileExplorer *explorer);
+void display_file_info(const FileEntry *file);
+int change_directory(FileExplorer *explorer, const char *dir_name);
+int go_parent_directory(FileExplorer *explorer);
+void format_file_size(LARGE_INTEGER size, char *buffer);
+void format_file_time(FILETIME *ft, char *buffer);
+void clear_screen(void);
+void show_help(void);
+int is_valid_path(const char *path);
+
+#endif // FILE_EXPLORER_H
