45a794e730886ef9f3ec1c99bf0ce893f4441c49
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2025-07-19 22:18:13 +0300

    Comments for clarity

--- Useful-Programs/Windows_File_Explorer/file_explorer.h
+++ Useful-Programs/Windows_File_Explorer/file_explorer.h
@@ -7,9 +7,11 @@
 #include <string.h>
 #include <time.h>
 
+// Constants
 #define MAX_PATH_LEN 260
 #define MAX_FILES 1000
 
+// Struct for file entry
 typedef struct {
     char name[MAX_PATH_LEN];
     char full_path[MAX_PATH_LEN];
@@ -19,12 +21,14 @@ typedef struct {
     int is_directory;
 } FileEntry;
 
+// Struct for file explorer
 typedef struct {
     char current_path[MAX_PATH_LEN];
     FileEntry files[MAX_FILES];
     int file_count;
 } FileExplorer;
 
+// Function protos
 void init_explorer(FileExplorer *explorer);
 int list_directory(FileExplorer *explorer, const char *path);
 void display_files(const FileExplorer *explorer);
