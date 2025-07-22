17e9433b283b261799ecd9a39f0b644c7cae32fb
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2025-07-22 08:16:53 +0300

    Add files via upload

---
+++ Larger-Projects/Windows_File_Metadata_Viewer/file_utils.h
@@ -0,0 +1,28 @@
+#ifndef FILE_UTILS_H
+#define FILE_UTILS_H
+
+#include <windows.h>
+#include <stdio.h>
+
+typedef struct {
+    char name[MAX_PATH];
+    char extension[32];
+    DWORD size_high;
+    DWORD size_low;
+    FILETIME creation_time;
+    FILETIME modified_time;
+    FILETIME access_time;
+    DWORD attributes;
+    int is_directory;
+    int is_hidden;
+    int is_readonly;
+    int is_system;
+} FileSystem;
+
+int file_exists(const char* filepath);
+int get_file_info(const char* filepath, FileInfo* info);
+void format_file_size(DWORD size_high, DWORD size_low, char* buffer, int buffer_size);
+void format_file_time(FILETIME* ft, char* buffer, int buffer_size);
+const char* get_file_type_description(const char* extension);
+
+#endif // FILE_UTILS_H
