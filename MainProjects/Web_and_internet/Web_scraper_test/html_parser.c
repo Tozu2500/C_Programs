5a0fb834e5bef7c69085255bf8926d37d8641729
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2025-08-05 02:03:23 +0300

    Update html_parser.c

--- MainProjects/Web_and_internet/Web_scraper_test/html_parser.c
+++ MainProjects/Web_and_internet/Web_scraper_test/html_parser.c
@@ -5,5 +5,23 @@
 #include <ctype.h>
 
 html_node_t* html_node_create(html_node_type_t type) {
-    
+    html_node_t* node = calloc(1, sizeof(html_node_t));
+    if (!node) return NULL;
+
+    node->type = type;
+    node->attribute_count = 0;
+    node->child_count = 0;
+    node->parent = NULL;
+
+    return node;
+}
+
+void html_node_free(html_node_t* node) {
+    if (!node) return;
+
+    for (int i = 0; i < node->child_count; i++) {
+        html_node_free(node->children[i]);
+    }
+
+    free(node);
 }
