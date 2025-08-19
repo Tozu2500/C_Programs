f32a234b7260605f47e3f9b1c3eb622b0d67578f
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2025-08-19 15:27:38 +0300

    Add files via upload

---
+++ MainProjects/Games/Terminal_pong_game/types.h
@@ -0,0 +1,15 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#define GAME_RUNNING 1
+#define GAME_PAUSED 2
+#define GAME_ENDED 3
+
+#define PLAYER_LEFT 1
+#define PLAYER_RIGHT 2
+
+#define EASY_DIFFICULTY 1
+#define MEDIUM_DIFFICULTY 2
+#define HARD_DIFFICULTY 3
+
+#define MIN_GAME_WIDTH 40
