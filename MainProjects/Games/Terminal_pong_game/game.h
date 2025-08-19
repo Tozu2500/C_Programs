1296317170aff939b4681377d76479e167fc564e
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2025-08-19 15:08:57 +0300

    Update game.h

--- MainProjects/Games/Terminal_pong_game/game.h
+++ MainProjects/Games/Terminal_pong_game/game.h
@@ -11,4 +11,29 @@ typedef struct {
     Paddle leftPaddle;
     Paddle rightPaddle;
     Score score;
+    int gameState;
+    int gameWidth;
+    int gameHeight;
+    int running;
+    int paused;
+    int winner;
+    int gameSpeed;
+    int difficulty;
+    int soundEnabled;
 } Game;
+
+void initGame(Game* game, int width, int height);
+void updateGame(Game* game);
+void resetGame(Game* game);
+void pauseGame(Game* game);
+void resumeGame(Game* game);
+void setGameSpeed(Game* game, int speed);
+void setDifficulty(Game* game, int difficulty);
+void toggleSound(Game* game);
+int isGameRunning(Game* game);
+int isGamePaused(Game* game);
+int getGameWinner(Game* game);
+void checkGameEnd(Game* game);
+void processGameLogic(Game* game);
+
+#endif // GAME_H
