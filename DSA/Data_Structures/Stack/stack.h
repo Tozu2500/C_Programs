f2145fc4ef2fa9d9a810bc9d976fb171c1c28766
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2026-01-28 19:58:55 +0200

    Add files via upload

---
+++ DSA/Data_Structures/Stack/stack.h
@@ -0,0 +1,11 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <stdbool.h>
+#include <stddef.h>
+
+// Initial capacity and growth factor for dynamic stack
+#define INITIAL_CAPACITY 10
+#define GROWTH_FACTOR 2
+
+#endif // STACK_H
